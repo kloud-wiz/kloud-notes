f5bd9954d7735807fd1ab669e3eec08c37e94231
Wisdom Emmanuel <emmawhiz.95@gmail.com>
2026-05-12 00:23:26 +0100

    feat: add dummy routes

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,20 @@ import express from "express"
 const app = express();
 
 app.get("/api/notes", (req, res) => {
-    res.send("server notes not yet added");
+    res.status(200).send("You have 50 notes");
 });
+
+app.post("/api/notes", (req, res) => {
+    res.status(201).json({message: "Note added successfully"});
+});
+
+app.put("/api/notes/:id", (req, res) => {
+    res.status(200).json({message: "Note updated successfully"})
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+    res.status(201).json({message: "Note deleted successfully"})
+})
 
 
 app.listen(5001, ()=>{
