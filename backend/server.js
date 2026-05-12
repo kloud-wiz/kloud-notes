e2758be89293bcd8880bf175a91f7b138afeadfe
Wisdom Emmanuel <emmawhiz.95@gmail.com>
2026-05-12 01:00:23 +0100

    feat: add express router to re-organize app routes

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,12 @@
 import express from "express"
+import notesRoute from "./routes/notesRoutes.js"
 
 //creates an Express application instance
 const app = express();
 
-app.get("/api/notes", (req, res) => {
-    res.status(200).send("You have 50 notes");
-});
+app.use("/api/notes", notesRoute);
 
-app.post("/api/notes", (req, res) => {
-    res.status(201).json({message: "Note added successfully"});
-});
-
-app.put("/api/notes/:id", (req, res) => {
-    res.status(200).json({message: "Note updated successfully"})
-});
-
-app.delete("/api/notes/:id", (req, res) => {
-    res.status(201).json({message: "Note deleted successfully"})
-})
-
-
-app.listen(5001, ()=>{
+app.listen(5001, () => {
     console.log("Server is listening on PORT 5001")
 });
 
