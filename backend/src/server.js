598ee4ae472f842487749387248413d7494c37bb
Wisdom Emmanuel <emmawhiz.95@gmail.com>
2026-05-13 01:58:50 +0100

    feat(backend): set db connection && env variables

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,19 @@
 import express from "express"
 import notesRoute from "./routes/notesRoutes.js"
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv"
+
+dotenv.config();
 
 //creates an Express application instance
 const app = express();
+const PORT = process.env.PORT || 5001;
+
+// Mongodb connection call
+connectDB();
 
 app.use("/api/notes", notesRoute);
 
-app.listen(5001, () => {
-    console.log("Server is listening on PORT 5001")
+app.listen(PORT, () => {
+    console.log(`Server is listening on PORT ${PORT}`)
 });
-
-
